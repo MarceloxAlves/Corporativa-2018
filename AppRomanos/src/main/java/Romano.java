bc21a878bc3cd122dd182a2aff663662e75ec009
Marcelo <marceloalvessoft@gmail.com>
2018-07-28 18:02:21 -0300

    Atividade TDD

--- AppRomanos/src/main/java/Romano.java
+++ AppRomanos/src/main/java/Romano.java
@@ -8,25 +8,65 @@ public class Romano {
     private Map<Character, Integer> algarismosBase;
 
     public Romano(String algarismos) {
-        this.algarismos = algarismos.toCharArray();
+        this.algarismos = algarismos.toUpperCase().toCharArray();
         algarismosBase = initializeAlgarismoBase();
-        this.validar();
+        this.validarNumero();
     }
 
-    private void validar() {
+    private void validarNumero() {
+        this.noEmpty();
+        this.caracterValidos();
+        this.noSequencia();
+        this.maiorSequenciaValida();
+        this.sequenciaAntesNumeroMaior();
+    }
+
+    private void caracterValidos(){
         for (int i=0; i < algarismos.length; i++) {
             if (!algarismosBase.containsKey(algarismos[i])) throw new IllegalArgumentException("Algarismo (" + algarismos[i] + ") inválido" );
         }
-        if (!sequenciaValida()) throw new IllegalArgumentException("Número de algarismos repetidos inválidos!");
     }
 
-    private boolean sequenciaValida(){
-        if(algarismos.length <= 3) return true;
-        if (maiorSequencia() <= 3) return true;
-        return false;
+    private void noEmpty(){
+
+        if (algarismos.length == 0) throw new IllegalArgumentException("Romano vazio");
     }
 
-    private int maiorSequencia(){
+    private void noSequencia(){
+        int maiorSequencia = 1;
+        int temp = 1;
+        for (int i=0; i < algarismos.length-1; i++) {
+            if (String.valueOf(algarismosBase.get(algarismos[i])).charAt(0) != '5') {
+                temp = 1;
+                continue;
+            }
+            if (algarismos[i] == algarismos[i + 1]) {
+                temp++;
+            } else {
+                temp = 1;
+            }
+            maiorSequencia = (int) MyMath.maior((double) maiorSequencia,(double) temp);
+            if (maiorSequencia > 1) throw new IllegalArgumentException("Seguencia de caracter repetidos eh inválidos!");
+
+        }
+    }
+
+    private void sequenciaAntesNumeroMaior(){
+        if(algarismos.length < 2) return;
+        int sequencia = 1;
+        int valorSequencia = algarismosBase.get(algarismos[0]);
+        for (int i=1; i < algarismos.length; i++) {
+            if (valorSequencia == algarismosBase.get(algarismos[i])){
+                sequencia++;
+            }else{
+                if(algarismosBase.get(algarismos[i]) > valorSequencia && sequencia > 1) throw new IllegalArgumentException("Seguencia  de caracteres repetidos antes de um numero maior eh inválida !");
+                valorSequencia =  algarismosBase.get(algarismos[i]);
+                sequencia = 1;
+            }
+        }
+    }
+
+    private void maiorSequenciaValida(){
         int maiorSequencia = 1;
         int temp = 1;
         for (int i=0; i < algarismos.length-1; i++) {
@@ -36,8 +76,8 @@ public class Romano {
                 temp = 1;
             }
             maiorSequencia = (int) MyMath.maior((double) maiorSequencia,(double) temp);
+            if (maiorSequencia > 3) throw new IllegalArgumentException("Seguencia de caracter repetidos eh inválidos!");
         }
-        return maiorSequencia;
     }
 
     public int converter() {
--- AppRomanos/src/test/java/TestAlgarismosRomano.java
+++ AppRomanos/src/test/java/TestAlgarismosRomano.java
@@ -1,37 +1,58 @@
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestAlgarismosRomano {
 
     Romano romano;
+    Map<Character, Integer> algarismoInicial;
 
     @Before
     public void setup(){
-        romano = new Romano("XV");
+         romano = new Romano("XV");
+        algarismoInicial = new HashMap<Character, Integer>();
+        algarismoInicial.put('I',1);
+        algarismoInicial.put('V',5);
+        algarismoInicial.put('X',10);
+        algarismoInicial.put('L',50);
+        algarismoInicial.put('C',100);
+        algarismoInicial.put('D',500);
+        algarismoInicial.put('M',1000);
     }
 
     @Test
-    public void test_algarismo_simples(){
-        romano = new Romano("X");
-        assertEquals(10,romano.converter());
+    public void deve_converter_romanos_para_arabicos_com_um_caracter_simples(){
+
+         algarismoInicial.forEach((k, v) -> {
+             romano = new Romano(String.valueOf(k));
+             assertEquals((int)v,romano.converter());
+         });
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deve_rejeitar_caracter_invalidos(){
+        romano = new Romano("*");
     }
 
     @Test
-    public void test_dois_algarismo(){
+    public void deve_converter_romanos_para_arabicos_com_dois_caracter(){
         romano = new Romano("IX");
         assertEquals(9,romano.converter());
     }
 
 
     @Test
-    public void test_dois_algarismo_iguais(){
+    public void deve_converter_romanos_para_arabicos_com_tres_caracter(){
         romano = new Romano("XII");
         assertEquals(12,romano.converter());
     }
 
     @Test
-    public void test_tres_algarismo_iguais(){
+    public void deve_converter_romanos_para_arabicos_com_tres_caracter_iguais(){
         romano = new Romano("VIII");
         assertEquals(8,romano.converter());
     }
@@ -48,21 +69,19 @@ public class TestAlgarismosRomano {
         assertEquals(349,romano.converter());
     }
 
-    @Test
-    public void test_unidade_milhar(){
-        romano = new Romano("MCDLXIX");
-        assertEquals(1469,romano.converter());
-    }
-
-
     @Test(expected = IllegalArgumentException.class)
     public void test_mais_de_tres_algarismos_repetidos(){
-        romano = new Romano("XICCXIIVVVVIX");
+        romano = new Romano("XICCXIIIIX");
     }
-
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_algarismos_invalidos(){
-        romano = new Romano("P");
+    public void test_algarismos_repetidos_invalidos(){
+        romano = new Romano("XICCXIIVVIX");
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deve_rejeita_pois_ha_uma_sequencia_antes_de_um_numero_maior(){
+        romano = new Romano("MMMCCCXXXIIIV");
+    }
+
 }
